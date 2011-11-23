a80e5e60c4719f4278cc461e69ca2f2818800708
schneider <schneider@blinkenlichts.net>
2011-11-23 23:33:55 +0100

    removed old code

--- software/ubd/src/cmdparser.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CMDPARSER_H_
-#define _CMDPARSER_H_
-#include <glib.h>
-#include <gio/gio.h>
-#include "net_tcp.h"
-void cmdparser_init(void);
-gssize cmdparser_cmd(gchar* cmd, gsize n, gchar** result);
-gboolean cmdparser_parse(struct nodebuffer *nb, gchar data);
-#endif
