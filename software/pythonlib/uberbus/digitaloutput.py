27550b7f25d070cc7157bc75a331f3d4d96ea54c
ircbot <ircbot@uberstar.muc.ccc.de>
2012-02-16 22:41:17 +0000

    restored broken digitaloutput

--- software/pythonlib/uberbus/digitaloutput.py
+++ software/pythonlib/uberbus/digitaloutput.py
@@ -1,6 +1,9 @@
 import ubnode
 
 class DigitalOutput(ubnode.UBNode):
+    udptype = '_digitaloutput._udp'
+    tcptype = '_digitaloutput._tcp'
+
     def __init__(self, address):
         ubnode.UBNode.__init__(self,address,2311)
 
@@ -11,3 +14,14 @@ class DigitalOutput(ubnode.UBNode):
     def clear(self, pin):
         cmd = "s %s 0"%pin
         return self.sendCommand(cmd)
+
+    def get(self, pin):
+        cmd = "g %s"%pin
+        if self.sendCommand(cmd):
+            return self.getMessage()
+        else:
+            return False
+
+class DigitalOutputResolver(ubresolver.UBResolver):
+    def __init__(self, udp=False):
+        ubresolver.UBResolver.__init__(self, DigitalOutput, udp)
